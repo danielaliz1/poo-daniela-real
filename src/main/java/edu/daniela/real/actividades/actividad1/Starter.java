33aeccd5a6e1f9fdf4c6508fe0208c59cc483170
danielaliz1 <daniela.liz.real@outlook.com>
2025-01-14 17:47:32 -0600

    commit1

---
+++ src/main/java/edu/daniela/real/actividades/actividad1/Starter.java
@@ -0,0 +1,9 @@
+package edu.daniela.real.actividades.actividad1;
+
+import edu.daniela.real.actividades.actividad1.ui.CLI;
+
+public class Starter {
+public static void main(String[] args) {
+        CLI.ShowMenu();
+    }
+}
---
+++ src/main/java/edu/daniela/real/actividades/actividad1/ui/CLI.java
@@ -0,0 +1,10 @@
+package edu.daniela.real.actividades.actividad1.ui;
+
+public class CLI {
+    public static void ShowMenu() {
+        System.out.println("Diagrama de clases: Carro");
+        System.out.println("1. Muestra carro");
+        System.out.println("2. Agrega un motor"); 
+    
+    }
+}
