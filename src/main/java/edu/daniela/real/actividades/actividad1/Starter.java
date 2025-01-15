3a5ed2b8f7c7b6661a0cf9bd995bc94bd2d0aefa
danielaliz1 <daniela.liz.real@outlook.com>
2025-01-15 11:59:58 -0600

    commit prueba

--- src/main/java/edu/daniela/real/actividades/actividad1/Starter.java
+++ src/main/java/edu/daniela/real/actividades/actividad1/Starter.java
@@ -1,9 +1,11 @@
 package edu.daniela.real.actividades.actividad1;
 
+
 import edu.daniela.real.actividades.actividad1.ui.CLI;
 
 public class Starter {
 public static void main(String[] args) {
-        CLI.ShowMenu();
+        CLI.Chilaquil();
     }
+
 }
